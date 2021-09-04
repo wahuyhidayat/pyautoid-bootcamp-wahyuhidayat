5f059210f13f159780c76cac325db64bd48a3e84
Wahyu Hidayat <why.hidayat98@gmail.com>
2021-09-04 22:40:35 +0700

    modify expected output

--- tugas01.py
+++ tugas01.py
@@ -9,6 +9,10 @@ url_list = ('https://noobtest.id/', 'https://erzaf.com/', 'https://www.orangsibe
 
 for url in url_list:
     driver.get(url)
-    print(driver.current_url, ' - ' , driver.title)
+    current_url = driver.current_url
+    delims = ['//', '/']
+    for d in delims:
+        current_url = current_url.replace(d, '-')
+    print(current_url.split('-')[1], ' - ' , driver.title)
 
 driver.close()
