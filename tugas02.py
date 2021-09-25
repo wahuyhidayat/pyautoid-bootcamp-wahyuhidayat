5eafc44b7a6cd7e7641dc51480006840f5077ac9
Wahyu Hidayat <why.hidayat98@gmail.com>
2021-09-25 23:47:43 +0700

    Refactor user input and for loop

--- tugas02.py
+++ tugas02.py
@@ -2,13 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-
-data_list = [
-    ('John','Snow','john@snow.com', 29, 2900000, 'Dark Knight'),
-    ('Arya','Stark','arya@stark.com', 19, 1900000, 'King Slayer'),
-    ('Sansa','Stark','sansa@stark.com', 25, 2500000, 'Princess')
-]
-
+data_dict = [
+    {'firstName': 'John', 'lastName':'Snow', 'userEmail':'john@snow.com', 'age':'29', 'salary':'2900000', 'department':'Dark Knight'},
+    {'firstName': 'Arya', 'lastName':'Stark', 'userEmail':'arya@stark.com', 'age':'19', 'salary':'1900000', 'department':'King Slayer'},
+    {'firstName': 'Sansa', 'lastName':'Stark', 'userEmail':'sansa@stark.com', 'age':'25', 'salary':'2500000', 'department':'Princess'}
+    ]
 
 driver = webdriver.Chrome()
 driver.maximize_window()
@@ -16,14 +14,10 @@ driver.implicitly_wait(10)
 
 driver.get("https://demoqa.com/webtables")
 
-for data in data_list:
+for data in data_dict:
     driver.find_element(By.ID, "addNewRecordButton").click()
-    driver.find_element(By.ID, "firstName").send_keys(data[0])
-    driver.find_element(By.ID, "lastName").send_keys(data[1])
-    driver.find_element(By.ID, "userEmail").send_keys(data[2])
-    driver.find_element(By.ID, "age").send_keys(data[3])
-    driver.find_element(By.ID, "salary").send_keys(data[4])
-    driver.find_element(By.ID, "department").send_keys(data[5])
+    for key in data:
+        driver.find_element(By.ID, key).send_keys(data[key])
     driver.find_element(By.ID, "submit").click()
 
 time.sleep(5)
